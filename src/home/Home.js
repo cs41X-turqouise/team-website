cca34585bf1213d7f00d9103f08f460ede4040cc
tr29038 <thmire2016@gmail.com>
2023-07-14 15:49:45 -0400

    Update website home page -- preliminary

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./Home.module.css";
+import logo from "./resources/background.png";
 
 const Home = () => {
     return (
-        <ul className={styles.main}>
-            <h1 className={styles.title}>{"<Project Name>"}</h1>
-            <h2 className={styles.title}>{"<Project Slogan>"}</h2>
-            <h2 className={styles.title}>{"CS 410"}</h2>
-            <h2 className={styles.title}>{"Team Turquoise"}</h2>
-        </ul>
+        <>
+            <ul className={styles.main}>
+                <h1 className={styles.title}>{"<Project Name>"}</h1>
+                <h2 className={styles.title}>{"<Project Slogan>"}</h2>
+                <h2 className={styles.title}>{"CS 410"}</h2>
+                <h2 className={styles.title}>{"Team Turquoise"}</h2>
+                <img src={logo} />
+            </ul>
+        </>
     );
 };
 
