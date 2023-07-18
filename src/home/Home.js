647e6024a2a0aa17c7f6d0282dcbb01362a583e2
Christian Woodington <cwood012@odu.edu>
2023-07-18 19:30:38 -0400

    Christian's Update

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Home.module.css";
 
+
+
 const Home = () => {
     return (
         <ul className={styles.main}>
             <h1 className={styles.title}>{"Sound Map for a Changing Landscape"}</h1>
-            <h2 className={styles.title}>{"Fighting Climate Change; One Sound Map at a Time"}</h2>
-            <h2 className={styles.title}>{"CS 410"}</h2>
-            <h2 className={styles.title}>{"Team Turquoise"}</h2>
+            <h2 className={styles.title}>{<i>"Fighting Climate Change; One Sound Map at a Time"</i>}</h2>
+            <h4 className={styles.title}>{"Team Turquoise"}</h4>
         </ul>
     );
 };
