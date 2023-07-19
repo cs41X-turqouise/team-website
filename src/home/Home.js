76c6cb65d11caf1b89e08e22f22cc1e1c54741cf
Christian Woodington <cwood012@odu.edu>
2023-07-18 22:27:26 -0400

    Christian Update to Website 4

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,9 +6,9 @@ import styles from "./Home.module.css";
 const Home = () => {
     return (
         <ul className={styles.main}>
-            <header_1 className={styles.title}>{"Sound Map for a Changing Landscape"}</header_1>
-            <header_2 className={styles.title}>{<i>"Fighting Climate Change; One Sound Map at a Time"</i>}</header_2>
-            <header_3 className={styles.title}>{"Team Turquoise"}</header_3>
+            <header_1 className={`${styles.title} ${styles.header_1}`}>{"Sound Map for a Changing Landscape"}</header_1>
+            <header_2 className={`${styles.title} ${styles.header_2}`}>{<i>"Fighting Climate Change; One Sound Map at a Time"</i>}</header_2>
+            <header_3 className={`${styles.title} ${styles.header_3}`}>{"Team Turquoise"}</header_3>
         </ul>
     );
 };
