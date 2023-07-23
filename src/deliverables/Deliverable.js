ec652f81bfdef2e8fd4a68cb9870cce21c95d14b
tr29038 <thmire2016@gmail.com>
2023-07-23 12:42:08 -0400

    * Fix deliverable images being stretched on mobile * Set direct competition bg color to black so it's legible

--- src/deliverables/Deliverable.js
+++ src/deliverables/Deliverable.js
@@ -4,7 +4,7 @@ import styles from "./Deliverable.module.css";
 const Deliverable = props => {
     return (
         <div className="grid grid-cols-1 border-4 border-black justify-self-center w-9/12 max-w-2xl" id={props.id}>
-            <img className="h-72 justify-self-center" src={props.image} />
+            <img className="h-72 justify-self-center object-contain bg-black" src={props.image} />
             <h1 className="text-center">{props.title}</h1>
         </div>
     );
