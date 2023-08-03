f782b492e7edc1e2599821cbb806a6b42f7a48d8
tr29038 <thmire2016@gmail.com>
2023-08-03 15:16:35 -0400

    Update glossary

--- src/glossary/Glossary.js
+++ src/glossary/Glossary.js
@@ -17,6 +17,10 @@ const words = [
         definition: "Any audio recording, image, artwork, video, news report, or personal experience a user or artist wishes to contribute to the sound map."
     },
     {
+        word: "Functionality",
+        definition: "The software should meet the goal and requirements of the user."
+    },
+    {
         word: "Digital Archive",
         definition: "A collection of different digital materials, such as artifacts, stored in a database."
     },
@@ -25,12 +29,36 @@ const words = [
         definition: "Short for Graphical User Interface, a visual way of interacting with a computer program."
     },
     {
+        word: "Maintainability",
+        definition: "The software should be easy to modify and update when needed."
+    },
+    {
         word: "Natural Soundscape",
         definition: "The acoustic environment of a particular location created by the aggregate of sounds that one can hear and associate with that environment."
     },
     {
         word: "Normalization",
         definition: "Converting differently formatted artifacts into a uniform, standard format that the system can effectively manage and display."
+    },
+    {
+        word: "Performance",
+        definition: "The software should have good speed, efficiency, and resource utilization."
+    },
+    {
+        word: "Reliability",
+        definition: "The software should be dependable and have minimum errors or bugs."
+    },
+    {
+        word: "Reusability",
+        definition: "The software should be able to reuse components or modules in different contexts or projects."
+    },
+    {
+        word: "Scalability",
+        definition: "The software should be able to handle increased workload or demand without compromising quality or performance."
+    },
+    {
+        word: "Security",
+        definition: "The software should be resistant to external attacks and protect the data and privacy of the user."
     },
     {
         word: "Socially Engaged Art",
@@ -43,6 +71,14 @@ const words = [
     {
         word: "Sprint",
         definition: "A set period of time during which a specific goal must be accomplished in the Agile Development process."
+    },
+    {
+        word: "Testability",
+        definition: "The software should be easy to test and verify for correctness and quality."
+    },
+    {
+        word: "Usability",
+        definition: "The software should be easy to learn and use, and have a user-friendly interface."
     }
 ];
 
