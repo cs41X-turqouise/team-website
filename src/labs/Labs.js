0bae96e9423421bd2346eefd204dddc24315103f
tr29038 <thmire2016@gmail.com>
2023-09-05 15:17:32 -0400

    Update outline

--- src/labs/Labs.js
+++ src/labs/Labs.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../misc/Header";
 import LabOutline from "./LabOutline";
 
-const labOutlineEmbed = <iframe className="h-3/4 w-2/5 bg-white" src="https://docs.google.com/document/d/e/2PACX-1vTXEo91EJTtY6Wp8fkQpVxKqNa0Iqs2dkZpuFk-u54ae0TERsKix3wbxbCRqWvITfbUeR2JauC-09Fu/pub?embedded=true"></iframe>;
+const labOutlineEmbed = <iframe className="h-3/4 w-2/5 bg-white" src="https://docs.google.com/document/d/e/2PACX-1vRjZ518UuUycZeqBn0_T2tSixevYt3uTjIaM-ekPpmGY-EdErJK_4mOVTdVnE63DzfuVG6Fqs50tkAY/pub?embedded=true"></iframe>;
 
 const labOutline = {
     title: "Lab 1 - Group Outline",
